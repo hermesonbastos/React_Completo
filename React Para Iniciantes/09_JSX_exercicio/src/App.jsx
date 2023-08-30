6a4d93462c169ed7ed719bf5d18ee8c380229a07
hermesonbastos <hermesonbastos@gmail.com>
2023-08-30 16:10:44 -0300

    Desafio JSX

--- React Para Iniciantes/09_JSX_exercicio/src/App.jsx
+++ React Para Iniciantes/09_JSX_exercicio/src/App.jsx
@@ -3,44 +3,8 @@
 
 //-------------------- Minha Resolução --------------------//
 // já inclui o conceito de componente ainda não visto no curso
-// import CardProduct from "./CardProduct";
+import CardProduct from "./CardProduct";
 
-// const produtos = [
-//   {
-//     id: 1,
-//     nome: "Smartphone",
-//     preco: "R$ 2000",
-//     cores: ["#29d8d5", "#252a34", "#fc3766"],
-//   },
-//   {
-//     id: 2,
-//     nome: "Notebook",
-//     preco: "R$ 3000",
-//     cores: ["#ffd045", "#d4394b", "#f37c59"],
-//   },
-//   {
-//     id: 3,
-//     nome: "Tablet",
-//     preco: "R$ 1500",
-//     cores: ["#365069", "#47c1c8", "#f95786"],
-//   },
-// ];
-
-// const App = () => {
-//   return (
-//     <>
-//       {produtos
-//         .filter((produto) => Number(produto.preco.replace("R$ ", "")) > 1500)
-//         .map((produto) => (
-//           <CardProduct produto={produto} />
-//         ))}
-//     </>
-//   );
-// };
-
-// export default App;
-
-//-------------------- Aula --------------------//
 const produtos = [
   {
     id: 1,
@@ -63,23 +27,59 @@ const produtos = [
 ];
 
 const App = () => {
-  // obs: o filter() não altera a estrutura dos valores (objetos)
-  // dentro array original, independente do valor que você escolheu utilizar
-  const dados = produtos.filter(
-    ({ preco }) => Number(preco.replace("R$ ", "")) > 1500,
+  return (
+    <>
+      {produtos
+        .filter((produto) => Number(produto.preco.replace("R$ ", "")) > 1500)
+        .map((produto) => (
+          <CardProduct produto={produto} />
+        ))}
+    </>
   );
-
-  return <section>
-    {dados.map(({ id, nome, preco, cores }) => (
-      <div key={id}>
-        <h1>{nome}</h1>
-        <p>Preço: {preco}</p>
-        <ul>{cores.map((cor) => (
-          <li style={{ backgroundColor: cor, color: "white" }} key={cor}>{cor}</li>
-        ))}</ul>
-      </div>
-    ))}
-  </section>;
 };
 
 export default App;
+
+// //-------------------- Aula --------------------//
+// const produtos = [
+//   {
+//     id: 1,
+//     nome: "Smartphone",
+//     preco: "R$ 2000",
+//     cores: ["#29d8d5", "#252a34", "#fc3766"],
+//   },
+//   {
+//     id: 2,
+//     nome: "Notebook",
+//     preco: "R$ 3000",
+//     cores: ["#ffd045", "#d4394b", "#f37c59"],
+//   },
+//   {
+//     id: 3,
+//     nome: "Tablet",
+//     preco: "R$ 1500",
+//     cores: ["#365069", "#47c1c8", "#f95786"],
+//   },
+// ];
+
+// const App = () => {
+//   // obs: o filter() não altera a estrutura dos valores (objetos)
+//   // dentro array original, independente do valor que você escolheu utilizar
+//   const dados = produtos.filter(
+//     ({ preco }) => Number(preco.replace("R$ ", "")) > 1500,
+//   );
+
+//   return <section>
+//     {dados.map(({ id, nome, preco, cores }) => (
+//       <div key={id}>
+//         <h1>{nome}</h1>
+//         <p>Preço: {preco}</p>
+//         <ul>{cores.map((cor) => (
+//           <li style={{ backgroundColor: cor, color: "white" }} key={cor}>{cor}</li>
+//         ))}</ul>
+//       </div>
+//     ))}
+//   </section>;
+// };
+
+// export default App;
