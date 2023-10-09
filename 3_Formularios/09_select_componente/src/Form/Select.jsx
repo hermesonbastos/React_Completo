e71e9bb1fc89f5961e49b9bcbd515152ccb8c5e2
hermesonbastos <hermesonbastos@gmail.com>
2023-10-09 09:21:58 -0300

    select componente

--- 3_Formularios/09_select_componente/src/Form/Select.jsx
+++ 3_Formularios/09_select_componente/src/Form/Select.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Select = ({ options, value, setValue }) => {
+const Select = ({ options, value, setValue, ...props }) => {
   return (
-    <select value={value} onChange={({target}) => setValue(target.value)}>
+    <select value={value} onChange={({target}) => setValue(target.value)} {...props}>
       <option value="" disabled>Selecione</option>
       {options.map(option => (
         <option key={option} value={option} >{option}</option>
