f1c6d58b592767146741620e2bdc4b0c63273175
hermesonbastos <hermesonbastos@gmail.com>
2024-01-09 00:21:46 -0300

    desafio router

---
+++ 5_React_Router/06_desafio_router/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Produtos from "./components/Produtos";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Contato from "./components/Contato";
+import Produto from "./components/Produto";
+
+const App = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Produtos />} />
+            <Route path="produto/:id" element={<Produto />} />
+            <Route path="contato" element={<Contato />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
