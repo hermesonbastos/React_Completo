8ef8e55493246ebe70fb4969dcd211972cf86118
hermesonbastos <hermesonbastos@gmail.com>
2024-01-09 16:04:48 -0300

    PropTypes

---
+++ 6_Mais_React/01_PropTypes/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from './Button'
+
+const App = () => {
+  return (
+    <div>
+      <Button margin="20px" width={300}>Clique Aqui</Button>
+    </div>
+  )
+}
+
+export default App
